2c55f172b90d2b03b7c2d9647382de1ae4cc32aa
Hamza <hamzaoubrahim51@gmail.com>
2023-12-03 10:40:03 +0100

    day 3: part two - done

--- days/code/3.py
+++ days/code/3.py
@@ -5,12 +5,12 @@ DAY = 3
 
 
 grid = loadinput(DAY).split('\n')
-cs = set()
-
+total = 0
 for y, line in enumerate(grid):
     for x, ch in enumerate(line):
-        if ch == '.' or ch.isdigit():
+        if ch != '*':
             continue
+        cs = set()
         for yd in [y - 1, y, y + 1]:
             for xd in [x - 1, x, x  + 1]:
                 if yd < 0 or yd >= len(grid) or xd < 0 or xd >= len(grid[yd]) or not grid[yd][xd].isdigit():
@@ -18,13 +18,15 @@ for y, line in enumerate(grid):
                 while xd > 0 and grid[yd][xd - 1].isdigit():
                     xd -= 1
                 cs.add((yd, xd))
+        if len(cs) != 2:
+            continue
+        ns = []
+        for r, c in cs:
+            s = ""
+            while c < len(grid[r]) and grid[r][c].isdigit():
+                s += grid[r][c]
+                c += 1
+            ns.append(int(s))
+        total += ns[0] * ns[1]
 
-ns = []
-for r, c in cs:
-    s = ""
-    while c < len(grid[r]) and grid[r][c].isdigit():
-        s += grid[r][c]
-        c += 1
-    ns.append(int(s))
-
-print(sum(ns))
+print(total)
